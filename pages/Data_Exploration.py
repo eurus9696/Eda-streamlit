b34e70b139f7b23729760bf2bef5fd999b195403
rumi <rumi@localhost.localdomain>
2023-07-28 01:40:42 +0530

    Made a prelim version of the test filtering feature

--- pages/Data_Exploration.py
+++ pages/Data_Exploration.py
@@ -1,2 +1,23 @@
 import streamlit as st
 import pandas as pd
+import util
+
+def main():
+    tests_df = pd.read_csv("tests.csv")
+    with st.sidebar:
+        tests,_ = util.filter_dataframe(tests_df)
+    try:
+        for test in tests.itertuples():
+            with st.expander(f"{test.Test_Name}"):
+                df,to_filter_columns = util.filter_dataframe(st.session_state.df,min=2,max=4,key=test.Test_Name)
+                df = df[list(to_filter_columns)]
+                st.dataframe(df)
+    except AttributeError as e:
+        st.header("No file uploaded yet!")
+        st.write("Upload dataframe and try again")
+
+if __name__ == '__main__':
+    util.page_config("Data Exploration")
+    util.add_bg()
+    util.add_title()
+    main()
